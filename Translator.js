05702d5c70fde0fe4f5095d9c7c6c01e711e2936
Ganga P Chandran <gangapchandran12@gmail.com>
2020-12-12 19:33:21 +0530

    all done

--- Translator.js
+++ Translator.js
@@ -1,25 +1,20 @@
-var Button=document.querySelector("#btn");
+var button=document.querySelector("#btn");
 var txtInput=document.querySelector("#inputText");
 var outputDiv=document.querySelector("#outputText");
 
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(input)
-{
-   return serverURL+"?"+"text="+input
-}
-function errorHandler(error)
-{
-   console.log("error is",error);
-   alert("Error struck,try again later")
-}
 function clickHandler(event)
 {
-   var inputText=txtInput.value;
-   var finalURL=getTranslationURL(inputText);
-   fetch(finalURL)
-     .this(response => response.json())
-     .this(json =>{ outputDiv.innerText=json.contents.translated;})
-     .catch(errorHandler())
-};
-Button.addEventListener("click",clickHandler);
+   fetch(getTranslationURL(txtInput.value))
+     .then(response => response.json())
+     .then(json => {
+          outputDiv.innerText=json.contents.translated;
+         })
+     .catch(() =>  alert("Error struck,try again later"))
+}
+function getTranslationURL(input)
+{
+    return serverURL+"?"+"text="+input
+}
+button.addEventListener("click",clickHandler);
